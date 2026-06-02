a6691e7a47f9e58221c1f0105430ee33fcabdf7b
mehedi-create <mehedi46x@gmail.com>
2026-06-03 01:25:45 +0600

    Delete public directory

--- public/player.js
+++
@@ -1,9 +0,0 @@
-const video = document.getElementById("video");
-
-video.addEventListener("loadedmetadata", () => {
-  console.log("Video Ready");
-});
-
-video.addEventListener("error", e => {
-  console.log(e);
-});
